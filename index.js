cb80d9bac393673dd496a55903dea38000050a5c
JoeMilius <joemilius@gmail.com>
2021-05-19 14:48:51 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove(main);
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "<h1>Joe is the champion</h1>";
